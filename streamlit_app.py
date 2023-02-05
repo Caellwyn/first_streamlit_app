6ce1345acc01a3401625eebf70a5604c489874f6
John Johnson <caellwyn@gmail.com>
2023-02-05 13:02:35 -0800

    Update streamlit_app.py
    import pandas and create fruit health table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -9,3 +10,6 @@ streamlit.text('🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🥝🥭Built Your Own Fruit Smoothie🍍🍓')
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
