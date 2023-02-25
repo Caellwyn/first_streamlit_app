4e832f43db806929bb45f88e40c99212eb57cf50
John Johnson <caellwyn@gmail.com>
2023-02-25 15:16:13 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,7 +73,7 @@ new_fruit = streamlit.text_input("Would you like to add a fruit?")
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     current_fruit = get_fruit_load_list()
-    streamlit.text(f'new fruit is {new_fruit}, current fruits are {current_fruit)}')
+    streamlit.text(f'new fruit is {new_fruit}, current fruits are {current_fruit}')
     if new_fruit not in list(current_fruit):
         thanks = insert_row_snowflake(new_fruit)
         streamlit.text(thanks)
