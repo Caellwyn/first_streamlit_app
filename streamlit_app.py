179caba9ac1af1397acdc36521d5e32b5d99932b
John Johnson <caellwyn@gmail.com>
2023-02-25 15:03:59 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor as my_cur:
+    with my_cnx.cursor() as my_cur:
         my_cur.execute(f"insert into pc_rivery_db.public.fruit_load_list values ('{new_fruit}')")
     return f'Thanks for adding {new_fruit}!'
 
